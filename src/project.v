699579bb43ae75cf7ed41433186c30676165e181
Abnerpfff1 <parra.abner@uabc.edu.mx>
2024-11-07 23:49:57 -0800

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_DIGI_OTA_3 (
+module tt_um_DIGI_OTA_4 (
     input  wire       VGND,
     input  wire       VDPWR,    // 1.8v power supply
 //    input  wire       VAPWR,    // 3.3v power supply
